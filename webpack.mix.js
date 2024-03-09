30ed49675bc620fcb90a8141c2ec08674bdb5383
mac <mac@MacBook-Pro.local>
2024-03-09 23:54:12 +0100

    adding tailwindcss

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,15 +1,30 @@
 const mix = require('laravel-mix');
+const path = require('path');
+const tailwindcss = require('tailwindcss');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
+mix.webpackConfig({
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'postcss-loader',
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            implementation: require('sass'),
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+});
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+    .options({
+        postCss: [tailwindcss],
+        processCssUrls: false,
+    });
